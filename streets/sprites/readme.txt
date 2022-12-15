9a089f910cbdd7c7e02f8f65a7ca1e84727f5f9c
jon <lolsonotcool@gmail.com>
2022-12-15 12:52:35 -0500

    In-editor animation test Fix sprite reset functionality Normalize editor collision positions to mouse coordinates Add readme.txt file, so I don't forget the commands

---
+++ streets/sprites/readme.txt
@@ -0,0 +1,40 @@
+SPRITE STUDIO README
+
+Sprites.py is the full studio
+
+
+STARTING WORK
+
+Starts by creating or loading a sprite sheet.
+The studio will start an editor for the sprite (16 x 32 px) in the top left corner of the sprite sheet image.
+
+
+EDITING SPRITES
+
+The sprite editor is 8x magnification. Select colors and click on the sprite editor to draw.
+Press LEFT to flip the editor image horizontally.
+Press UP to flip the editor image vertically.
+Press SHIFT+LEFT or SHIFT+RIGHT to rotate the sprite image in the editor. Results not guaranteed.
+Press ESCAPE to reset sprite in editor to the sprite sheet image.
+
+Press ENTER to save the focused sprite editor to the sprite sheet, you will be prompted for a tag.
+The tag identifies which animation series the sprite belongs to like "walk_right"
+Press RIGHT to open a new editor with a copy of the sprite and its tag one position to the right.
+Press SPACE to play the animation series an editor is working on.
+Press DOWN to open a new editor with a copy of the sprite to a new series in the next sprite sheet row.
+
+
+NAVIGATING THE SPRITE SHEET
+
+Pressing BACK-QUOTE (`) will prompt for a tag to search, and then show the entire sheet.
+All sprites with tags partially matching the search term will be highlighted.
+Press BACK-QUOTE again to enter a new search term.
+Click on any sprite position to clear all open editors and open a new editor for the selected sprite.
+Press ESCAPE to exit from the navigation page without changing open editors.
+
+
+SAVING THE SPRITE SHEET
+
+Press any other key or click close on the window to prompt for a file name and save the sprite sheet and tags.
+Press ENTER on the prompt to save.
+Press ESCAPE or click to close the window again to exit without saving.
--- streets/sprites/sprites.py
+++ streets/sprites/sprites.py
@@ -9,6 +9,8 @@ class Editor:
     # need to start bundling data together
     # connection between editor and sprite on sheet must be intimate
     def __init__(self, sprite, editor, copy_editor=None):
+        self.animation = None
+        self.frame_count = 0
         self.sprite = sprite
         self.surface, self.tag = self.get_sprite()
         if copy_editor is not None and self.tag is None:
@@ -17,9 +19,19 @@ class Editor:
         self.rect = self.surface.get_rect()
         self.rect.x, self.rect.y = editor.x, editor.y
 
+        # introduced to compensate for drawing surface being 32 px below top of window
+        self.click_rect = pygame.Rect(self.rect)
+        self.click_rect.y += 32
+
     def get_sprite(self):
+        if self.animation:
+            self.frame_count = (self.frame_count + 1) % (len(self.animation) * 10)
+            frame = self.frame_count//10
+            my_sprite = self.animation[frame]
+        else:
+            my_sprite = pygame.Rect(self.sprite)
         lil_editor = pygame.Surface(studio.SPRITE_SIZE)
-        lil_editor.blit(sprite_sheet, (0, 0), self.sprite)
+        lil_editor.blit(sprite_sheet, (0, 0), my_sprite)
         return pygame.transform.scale(lil_editor, studio.EDITOR_SIZE), sprite_meta.get(tuple(self.sprite))
 
     def save_sprite(self):
@@ -40,7 +52,38 @@ class Editor:
         pixel = pygame.Surface((studio.EDITOR_PIXEL, studio.EDITOR_PIXEL))
         pixel.fill(colour)
         self.surface.blit(pixel, (px_x, px_y))
-        work.blit(self.surface, self.rect)
+
+    def animate(self):
+        if self.animation:
+            self.reset()
+        else:
+            tag_list = list(sprite_meta.values())
+            pos_list = list(sprite_meta.keys())
+            index_list = []
+            while tag_list.count(self.tag):
+                i = tag_list.index(self.tag)
+                index_list.append(pos_list[i])
+                tag_list = tag_list[i+1:]
+                pos_list = pos_list[i+1:]
+            self.animation = index_list
+
+    def reset(self):
+        if self.animation:
+            self.animation.clear()
+        self.surface, _ = self.get_sprite()
+
+    def draw(self):
+        pygame.draw.rect(work, studio.COLOUR_KEY, self.rect)
+        if self.animation:
+            self.surface, _ = self.get_sprite()
+        work.blit(self.surface, (self.rect.x, self.rect.y))
+
+    def flip(self, x_flip, y_flip):
+        self.surface = pygame.transform.flip(self.surface, x_flip, y_flip)
+
+    def rotate(self, angle):
+        rotated = pygame.transform.rotate(self.surface, angle)
+        self.surface.blit(rotated, (0, 0), pygame.Rect((30, 15), studio.EDITOR_SIZE))
 
 
 def next_sprite_column(editor):
@@ -51,10 +94,9 @@ def next_sprite_column(editor):
     new_sprite_pos.x += studio.SPRITE_SIZE[0]
     new_editor_pos = pygame.Rect(editor.rect)
     new_editor_pos.x += studio.EDITOR_SIZE[0] + 32
-    if studio.SHEET_SIZE[0] > new_editor_pos.x + studio.EDITOR_SIZE[0] \
-            and studio.SHEET_SIZE[0] > new_sprite_pos.x + studio.SPRITE_SIZE[0]:
+    if studio.SHEET_SIZE[0] > new_editor_pos.x + studio.EDITOR_SIZE[0]:
         new_editor = Editor(new_sprite_pos, new_editor_pos, editor)
-        editors[tuple(new_editor.rect)] = new_editor
+        editors[tuple(new_editor.click_rect)] = new_editor
         return new_editor
     return None
 
@@ -67,10 +109,9 @@ def next_sprite_row(editor):
     new_sprite_pos.y += studio.SPRITE_SIZE[1]
     new_editor_pos = pygame.Rect(editor.rect)
     new_editor_pos.y += studio.EDITOR_SIZE[1] + 32
-    if studio.SHEET_SIZE[1] > new_editor_pos.y + studio.EDITOR_SIZE[1] \
-            and studio.SHEET_SIZE[1] > new_sprite_pos.y + studio.SPRITE_SIZE[1]:
+    if studio.SHEET_SIZE[1] > new_editor_pos.y + studio.EDITOR_SIZE[1]:
         new_editor = Editor(new_sprite_pos, new_editor_pos, editor)
-        editors[tuple(new_editor.rect)] = new_editor
+        editors[tuple(new_editor.click_rect)] = new_editor
         return new_editor
     return None
 
@@ -84,7 +125,7 @@ if __name__ == '__main__':
     sprite_pos = pygame.Rect((0, 0), studio.SPRITE_SIZE)
     editor_pos = pygame.Rect((16, 32), studio.EDITOR_SIZE)
     focus_editor = Editor(sprite_pos, editor_pos)
-    editors[tuple(focus_editor.rect)] = focus_editor
+    editors[tuple(focus_editor.click_rect)] = focus_editor
 
     overlay = pygame.Surface(WORK_SIZE)
     overlay = overlay.convert_alpha()
@@ -105,7 +146,7 @@ if __name__ == '__main__':
         studio.clock.tick(60)
         # draw active editors
         for v in list(editors.values()):
-            work.blit(v.surface, (v.rect.x, v.rect.y))
+            v.draw()
         # cover work surface with tint
         overlay.fill(studio.SCREEN_COLOUR)
         if focus_editor is not None:
@@ -117,11 +158,11 @@ if __name__ == '__main__':
         pygame.display.flip()
 
         mouse_x, mouse_y = pygame.mouse.get_pos()
-        if 32 > mouse_y and not status_shown:
+        if 32 > mouse_y:
             studio.draw_colour_bar()
 
         # check for selecting a colour from the palette
-        mouse_rect = pygame.Rect(pygame.mouse.get_pos(), (1, 1))
+        mouse_rect = pygame.Rect((mouse_x, mouse_y), (1, 1))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -129,13 +170,14 @@ if __name__ == '__main__':
                 editing = False
                 sheets.save_sprite_sheet(sprite_sheet, sprite_meta, save_filename)
             if event.type == pygame.KEYDOWN:
+                mods = pygame.key.get_mods()
                 if status_shown:
                     # if we show a status, dismiss with any key
                     status_shown = studio.clear_status_text()
                 elif event.key == pygame.K_ESCAPE:
                     if focus_editor:
                         # escape resets current editor
-                        focus_editor.get_sprite()
+                        focus_editor.reset()
                         status_shown = studio.draw_status_text(
                             f'Reset edits to sprite {tuple(focus_editor.sprite)}', studio.STATUS_YELLOW)
                 elif event.key == pygame.K_BACKQUOTE:
@@ -149,16 +191,27 @@ if __name__ == '__main__':
                         editors.clear()
                         editor_pos = pygame.Rect((16, 32), studio.EDITOR_SIZE)
                         focus_editor = Editor(jump_sprite, editor_pos, focus_editor)
-                        editors[tuple(focus_editor.rect)] = focus_editor
+                        editors[tuple(focus_editor.click_rect)] = focus_editor
                 elif event.key == pygame.K_RETURN:
                     sprite_meta[tuple(focus_editor.sprite)] = focus_editor.save_sprite()
                     status_shown = studio.draw_status_text(
                         f'Saved sprite {sprite_meta[tuple(focus_editor.sprite)]}', studio.STATUS_GREEN)
+
+                elif focus_editor and event.key == pygame.K_UP:
+                    focus_editor.flip(False, True)
+                elif focus_editor and event.key == pygame.K_LEFT:
+                    if mods == 1 or mods == 2:
+                        focus_editor.rotate(15)
+                    else:
+                        focus_editor.flip(True, False)
                 elif focus_editor and event.key == pygame.K_RIGHT:
-                    # create next frame, carry tag to new frame if possible
-                    focus_editor = next_sprite_column(focus_editor)
-                    if focus_editor is None:
-                        status_shown = studio.draw_status_text(f'No room on this row', studio.STATUS_RED)
+                    if mods == 1 or mods == 2:
+                        focus_editor.rotate(-15)
+                    else:
+                        # create next frame, carry tag to new frame if possible
+                        focus_editor = next_sprite_column(focus_editor)
+                        if focus_editor is None:
+                            status_shown = studio.draw_status_text(f'No room on this row', studio.STATUS_RED)
                 elif focus_editor and event.key == pygame.K_DOWN:
                     # create new frame down, tags should only run horizontally for animation
                     new_tag = studio.get_text_input('Enter tag for new series: ', None)
@@ -167,7 +220,11 @@ if __name__ == '__main__':
                         focus_editor.tag = new_tag
                     if focus_editor is None:
                         status_shown = studio.draw_status_text(f'No room on this column', studio.STATUS_RED)
-                else:
+
+                elif focus_editor and event.key == pygame.K_SPACE:
+                    focus_editor.animate()
+
+                elif not (event.key == pygame.K_LSHIFT or event.key == pygame.K_RSHIFT):
                     studio.screen.blit(sprite_sheet, (0, 0))
                     new_filename = sheets.save_sprite_sheet(sprite_sheet, sprite_meta, save_filename)
                     status_shown = True
@@ -195,5 +252,4 @@ if __name__ == '__main__':
                 if draw_colour:
                     focus_editor.paint(draw_colour)
                     status_shown = studio.draw_status_text(f'ENTER to save or ESCAPE to reset', draw_colour)
-
     pygame.quit()
